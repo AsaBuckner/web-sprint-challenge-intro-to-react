3a5ed0f053b15dc2d8d2d629d3ff3d770d0f0b21
Xavier Buckner <xavier@Xaviers-MacBook-Pro.local>
2022-11-01 12:34:38 -0500

    Fin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Character from './components/Character';
+import "./App.css";
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +11,27 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const CharacterWrapper = styled.div`
+  
+    display: flex;
+    justify-content: center;
+  
+  `;
+
+
+
   return (
+
     <div className="App">
       <h1 className="Header">Characters</h1>
+      
+      <CharacterWrapper>
+        <Character />
+      </CharacterWrapper>
+
+
     </div>
+
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,76 @@
 // Write your Character component here
+
+import React, {useEffect, useState} from "react"
+import styled from "styled-components"
+import axios from "axios"
+import "../App.css";
+
+
+
+
+
+const Character = () => {
+
+const [characterData, setCharacterData] = useState(null)
+const [openDiv, setDiv] = useState(false)
+
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people/')
+        .then((res)=>{
+            setCharacterData(res.data)
+            console.log(res.data)
+            
+        })
+        
+    }, [])
+
+    const toggleOpen = () => {
+        setDiv(!openDiv)
+        console.log(openDiv)
+    }
+
+    const Wrapper = styled.div`
+    width: 90%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-content: center;
+    background: white
+    `;
+
+
+    const DetailsWrapper = styled.div`
+
+    display: flex
+    background : white
+    `;
+
+    return (
+    <div>
+        {characterData.map((character)=>{
+        return(
+            <div>
+            <Wrapper>
+                {character.name}
+            </Wrapper>
+          <DetailsWrapper>
+          <p>{character.birth_year}</p>
+          <p>{character.gender}</p>
+          <p>{character.height}</p>
+          <p>{character.mass}</p>
+          <p>{character.skin_color}</p>
+          <p>{character.hair_color}</p>
+          <p>{character.eye_color}</p>
+          </DetailsWrapper>
+        </div>
+        )
+        })}
+    </div>
+    )
+    }
+
+
+
+
+
+export default Character
